ccada3b8f41752943e6afd57045f2de05d5434ee
mmartinalo <marioma87@gmail.com>
2014-12-17 18:33:23 +0100

    Fix get Max and Min. Add new methods for get dinstences between points

--- src/WavesInterpreter/WaveInterpreterUtils.php
+++ src/WavesInterpreter/WaveInterpreterUtils.php
@@ -4,6 +4,7 @@
 namespace WavesInterpreter;
 
 
+use WavesInterpreter\Exception\WavePointException;
 use WavesInterpreter\Point\Point;
 
 /**
@@ -110,6 +111,7 @@ final class WaveInterpreterUtils {
             //Nos hacen falta al menos dos puntos para poder comparar
             if(is_null($lastPoint)){
                 $maxPoint = $currentPoint;
+                $lastPoint = $currentPoint;
                 continue;
             }
 
@@ -118,9 +120,97 @@ final class WaveInterpreterUtils {
                 $maxPoint = $currentPoint;
             }
 
+            $lastPoint = $currentPoint;
+
         }
 
-        //Si el array no contiene elementos devolverá null ya que no entra ene l foreach
+        //Si el array no contiene elementos devolverá null ya que no entra en el foreach
         return $maxPoint;
+    }
+
+    /**
+     * Dado un array de Points obtiene el punto mínimo de todos ellos
+     *
+     * @param array $points
+     * @return null|\WavesInterpreter\Point\Point
+     */
+    static function getMinPointTrail(array $points){
+
+        //Si solo tenemos un punto, es el máximo
+        if(count($points) == 1){
+            return $points[0];
+        }
+
+        /** @var Point $lastPoint */
+        $lastPoint = null;
+        /** @var Point $maxPoint */
+        $minPoint = null;
+        /** @var Point $currentPoint */
+        foreach($points as $currentPoint){
+
+            //Nos hacen falta al menos dos puntos para poder comparar
+            if(is_null($lastPoint)){
+                $minPoint = $currentPoint;
+                $lastPoint = $currentPoint;
+                continue;
+            }
+
+            //Si el actual es menor o igual seteamos este como el mayor. En caso de igualdad nos quedamos con el último
+            if($currentPoint->getY() <= $lastPoint->getY()){
+                $minPoint = $currentPoint;
+            }
+
+            $lastPoint = $currentPoint;
+        }
+
+        //Si el array no contiene elementos devolverá null ya que no entra en el foreach
+        return $minPoint;
+    }
+
+    /**
+     * Este helper no ordena! devuelve la distancia entre el primer y último elemento
+     *
+     * @param array $points
+     * @throws WavePointException
+     * @return null|int
+     */
+    static public  function getXDistance(array $points = array()){
+
+        if(!count($points)){
+            return 0;
+        }
+
+        //Solo podemos devolver una distancia si es un array de por lo menos dos elementos
+        if(count($points)  == 2 ){
+            return 1;
+        }
+
+        $firstPoint = array_slice($points,0,1)[0];
+        $lastPoint = array_slice($points,-1,1)[0];
+
+        if(!$firstPoint instanceof Point || !$lastPoint instanceof Point){
+            throw new WavePointException("Error obteniendo la distancia X entre puntos");
+        }
+
+        return abs($lastPoint->getX() - $firstPoint->getX());
+    }
+
+    /**
+     * Devuelve la distancia entre el elemento con mayot potencial y el que menos
+     *
+     * @param array $points
+     * @throws WavePointException
+     * @return null|int
+     */
+    static public  function getYDistance(array $points = array()){
+
+        $minPoint = self::getMinPointTrail($points);
+        $maxPoint = self::getMaxPointTrail($points);
+
+        if(!$minPoint instanceof Point || !$maxPoint instanceof Point){
+            return 0;
+        }
+
+        return $maxPoint->getY() - $minPoint->getY();
     }
 } 
